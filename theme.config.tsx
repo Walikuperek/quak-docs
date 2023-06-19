aa3c9eaba585d264797ec6a5415cea366278b90e
Kacper Walczak <kacperwalczak94@gmail.com>
2023-06-20 00:07:23 +0200

    feat(home, learn, libs): init + libs done

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,32 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <div style={{display: 'flex', alignItems: 'center', fontWeight: 700, letterSpacing: '2px'}}><img src="https://quak.com.pl/quak_logo_72x72.png" alt="QUAK Logo" width={34} />&nbsp;QUAK</div>,
+  primaryHue: 320,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="QUAK" />
+      <meta property="og:description" content="QUAK Kacper Walczak company website" />
+    </>
+  ),
+  
+  search: {
+    placeholder: 'Search...',
+  },
+  banner: {
+    text: 'QUAK 🚀 Programming, Philosophy, Knowledge',
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Walikuperek/quak-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Walikuperek/quak-docs',
+  // TODO: Add chat link
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   footer: {
-    text: 'Nextra Docs Template',
+    text: `QUAK Kacper Walczak © ${new Date().getFullYear()}`,
   },
 }
 
