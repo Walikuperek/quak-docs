4386896f9202409f359345dbe3e5397092484e94
Kacper Walczak <kacperwalczak94@gmail.com>
2023-06-22 01:17:05 +0200

    feat(learn): added abcde init

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = {
+  ...withNextra(),
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  }
+}
