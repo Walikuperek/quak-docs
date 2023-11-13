b5279584028a2a0679684ba608f3e30f51611ca6
kacper walczak <kacperwalczak@kacpers-MacBook-Pro-2.local>
2023-11-13 12:45:37 +0100

    feat(update): update

--- data/tag-posts/typescript.tsx
+++ data/tag-posts/typescript.tsx
@@ -1,4 +1,5 @@
 import {TSTag} from "../../components/tags/typescript";
+import {newestUpSorter} from "../sorter";
 
 export const TypescriptPosts = [
     {
@@ -25,4 +26,31 @@ export const TypescriptPosts = [
         description: "Learn how to implement Dependency Injection in TS.",
         label: <></>
     },
+    {
+        color: "typescript",
+        category: "frontend",
+        link: "/learn/typescript/discriminated-union",
+        tag: <TSTag disabled={true}/>,
+        image: <section className="flex align-center justify-center gap-3 h-full overflow-hidden">
+            <div className="bg-violet-500 h-40 w-40 p-1 mt-20 text-white font-bold">
+            <span className="text-lg">
+                $ buyProduct<br/>
+                <span className="text-violet-300">&middot; vipCode?</span>
+            </span>
+            </div>
+            <div className="flex flex-col">
+                <div className="bg-fuchsia-600 h-10 w-20 p-1 mt-20 text-white font-bold">VIPUser</div>
+                <div className="bg-fuchsia-600 h-10 w-20 p-1 mt-1 text-white font-bold">User</div>
+            </div>
+        </section>,
+        author: "Kacper Walczak",
+        createdAt: "08-11-2023",
+        title: <>Discriminated Unions</>,
+        descriptionTextLines: [
+            "Learn how to deal with optional properties in typescript.",
+        ],
+        description: "Learn how to deal with optional properties in typescript.",
+        label: <></>
+    },
 ];
+newestUpSorter(TypescriptPosts);
